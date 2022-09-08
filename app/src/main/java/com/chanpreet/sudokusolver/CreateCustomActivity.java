0db250f82583404de43b0330644fffa432c80bef
Chanpreet Singh <chanpreet3000@gmail.com>
2022-09-08 23:26:17 +0530

    Sudoku Custom Creator

---
+++ app/src/main/java/com/chanpreet/sudokusolver/CreateCustomActivity.java
@@ -0,0 +1,14 @@
+package com.chanpreet.sudokusolver;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CreateCustomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_custom);
+    }
+}
